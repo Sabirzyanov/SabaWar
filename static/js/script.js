d7e6e42447b44c6da0f0e481149021a6aaf6ecbb
KaPToHka <57670787+Sabirzyanov@users.noreply.github.com>
2022-06-13 20:25:39 +0300

    Created but tickets modal block

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,39 @@
+/* Pretty anchors system */
 const anchors = document.querySelectorAll('a[href*="#"]')
 for (let anchor of anchors) {
     anchor.addEventListener('click', function (e) {
         e.preventDefault()
 
+        // Search needed anchor
         const blockID = anchor.getAttribute('href').substr(1)
 
+        // Pretty scroll to anchor
         document.getElementById(blockID).scrollIntoView({
             behavior: 'smooth',
             block: 'start'
         })
     })
+}
+
+/* Modal window controller */
+var modal = document.getElementById("buy-ticket-modal");
+var modalOpen = document.getElementById("buy-ticket-open-button");
+var modalExit = document.getElementById("buy-ticket-close-button");
+
+// Open modal
+modalOpen.onclick = () => {
+    modal.style.display = "flex";
+    console.log(123);
+}
+
+// Exit from modal, when user click on close button
+modalExit.onclick = () => {
+    modal.style.display = "none";
+}
+
+// Exit from modal, when user click outside of modal
+window.onclick = (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
